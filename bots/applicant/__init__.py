699beaf7196782e8f5134f4cd5d454e7902c4d5c
xiidot1303 <xii1303@inbox.ru>
2022-08-26 14:44:10 +0500

    change gitignore and add __init__.py

---
+++ bots/applicant/__init__.py
@@ -0,0 +1,5 @@
+from bots.applicant.utils import *
+from bots.applicant.deco import *
+from bots.applicant.conversationList import *
+from bots.strings import lang_dict
+
---
+++ bots/supplier/__init__.py
@@ -0,0 +1,4 @@
+from bots.supplier.utils import *
+from bots.supplier.deco import *
+from bots.supplier.conversationList import *
+from bots.strings import lang_dict
